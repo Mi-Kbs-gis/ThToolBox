7c19b43cce8b3b5b41abfd1524292801393273f1
Michael Kürbs <25895003+Mi-Kbs-gis@users.noreply.github.com>
2023-12-22 14:01:40 +0100

    Version 2.14

--- help/source/conf.py
+++ help/source/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# TlugProcessingPlugin documentation build configuration file, created by
+# ThToolBoxPlugin documentation build configuration file, created by
 # sphinx-quickstart on Sun Feb 12 17:11:03 2012.
 #
 # This file is execfile()d with the current directory set to its containing dir.
@@ -40,8 +40,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'TlugProcessingPlugin'
-copyright = u'2013, Michael Kürbs by Thüringer Landesanstalt für Umwelt und Geologie (TLUG)'
+project = u'ThToolBoxPlugin'
+copyright = u'2013, Michael Kürbs by Thüringer Landesamt für Umwelt, Bergbau und Naturschutz (TLUBN)'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -178,8 +178,8 @@ htmlhelp_basename = 'TemplateClassdoc'
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'TlugProcessingPlugin.tex', u'TlugProcessingPlugin Documentation',
-   u'Michael Kürbs by Thüringer Landesanstalt für Umwelt und Geologie (TLUG)', 'manual'),
+  ('index', 'ThToolBoxPlugin.tex', u'ThToolBoxPlugin Documentation',
+   u'Michael Kürbs by Thüringer Landesamt für Umwelt, Bergbau und Naturschutz (TLUBN)', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -211,6 +211,6 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'TemplateClass', u'TlugProcessingPlugin Documentation',
-     [u'Michael Kürbs by Thüringer Landesanstalt für Umwelt und Geologie (TLUG)'], 1)
+    ('index', 'TemplateClass', u'ThToolBoxPlugin Documentation',
+     [u'Michael Kürbs by Thüringer Landesamt für Umwelt, Bergbau und Naturschutz (TLUBN)'], 1)
 ]
